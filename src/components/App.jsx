67d69938b8fd833083dd4c29665a003d203fdb80
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-12-13 19:07:09 +1100

    feat(Resume): Fill full Resume with dummy content
    Whilst the content now filling the resume is
    dummy content it has been provided to the Resume
    component passed down through props. This will
    mean that the details section with forms I create
    later on will have access to this data also, which
    will make it easy to change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,11 +47,59 @@ export default function App() {
     }
   ])
 
+  const [educationInfo, setEducationInfo] = useState([
+    {
+      id: 0,
+      school: 'University Name',
+      startDate: 'Oct.2017',
+      endDate: 'Mar.2021',
+      degree: 'Computer Science',
+      location: 'City, Street',
+      details: [
+        'List your honors like summa cum laude or Economics Honors Society',
+        'Studied abroad in City, Country during spring/fall 20YY semester (University Name)',
+        'Any other fun stuff like varsity sports, fraternity/sorority, or something that gives you a little color'
+      ]
+    },
+    {
+      id: 1,
+      school: 'University Name',
+      startDate: 'Oct.2017',
+      endDate: 'Mar.2021',
+      degree: 'Computer Science',
+      location: 'City, Street',
+      details: [
+        'List your honors like summa cum laude or Economics Honors Society',
+        'Studied abroad in City, Country during spring/fall 20YY semester (University Name)',
+        'Any other fun stuff like varsity sports, fraternity/sorority, or something that gives you a little color'
+      ]
+    },
+    {
+      id: 2,
+      school: 'University Name',
+      startDate: 'Oct.2017',
+      endDate: 'Mar.2021',
+      degree: 'Computer Science',
+      location: 'City, Street',
+      details: [
+        'List your honors like summa cum laude or Economics Honors Society',
+        'Studied abroad in City, Country during spring/fall 20YY semester (University Name)',
+        'Any other fun stuff like varsity sports, fraternity/sorority, or something that gives you a little color'
+      ]
+    },
+  ])
+
+  const [skills, setSkills] = useState([
+    'Strategic planning', 'strategic partnerships', 'revenue modeling & forecasting', 
+    'retail partnerships', 'sales, & distro', 'contract negotiations', 'account management', 
+    'Amazon marketing', 'channel marketing', 'crowdfunding'
+  ])
+
   return (
     <div className='main-layout'>
       <Sidebar />
       <Details />
-      <Resume generalInfo={generalInfo} experienceInfo={experienceInfo}/>
+      <Resume generalInfo={generalInfo} experienceInfo={experienceInfo} educationInfo={educationInfo} skills={skills}/>
     </div>
   )
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 import '/src/styles/Resume.css'
 
-export default function Resume({ generalInfo, experienceInfo }) {
+export default function Resume({ generalInfo, experienceInfo, educationInfo, skills }) {
     return (
         <div className='resume-container'>
+            <div className='overlay'></div>
             <div className='resume-header'>
                 <h1>{generalInfo.name}</h1>
                 <div>
@@ -18,29 +19,58 @@ export default function Resume({ generalInfo, experienceInfo }) {
                 <hr />
                 {experienceInfo.map((job) => {
                     return (
-                        <div key={job.id} className='job-card'>
-                        <div>
-                            <h3>{job.company}</h3>
-                            <span>{job.startDate} - {job.endDate}</span>
+                        <div key={job.id} className='card'>
+                            <div>
+                                <h3>{job.company}</h3>
+                                <span>{job.startDate} - {job.endDate}</span>
+                            </div>
+                            <div>
+                                <span><em>{job.title}</em></span><address><em>{job.location}</em></address>
+                            </div>
+                            <ul>
+                                {job.response.map((item) => {
+                                    return (
+                                        <li key={crypto.randomUUID()}>{item}</li>
+                                    )
+                                })}
+                                {job.achievements.map((item) => {
+                                    return (
+                                        <li key={crypto.randomUUID()}>{item}</li>
+                                    )
+                                })}
+                            </ul>
                         </div>
-                        <div>
-                            <span><em>{job.title}</em></span><address><em>{job.location}</em></address>
-                        </div>
-                        <ul>
-                            {job.response.map((item) => {
-                                return (
-                                    <li key={crypto.randomUUID()}>{item}</li>
-                                )
-                            })}
-                            {job.achievements.map((item) => {
-                                return (
-                                    <li key={crypto.randomUUID()}>{item}</li>
-                                )
-                            })}
-                        </ul>
-                    </div>
                     )
                 })}
+            </div>
+            <div className='resume-education'>
+                <h2>Education</h2>
+                <hr />
+                {educationInfo.map((entry) => {
+                    return (
+                        <div className='card' key={entry.id}>
+                            <div>
+                                <h3>{entry.school}</h3>
+                                <span>{entry.startDate} - {entry.endDate}</span>
+                            </div>
+                            <div>
+                                <span><em>{entry.degree}</em></span><address><em>{entry.location}</em></address>
+                            </div>
+                            <ul>
+                                {entry.details.map((item) => {
+                                    return (
+                                        <li>{item}</li>
+                                    )
+                                })}
+                            </ul>
+                        </div>
+                    );
+                })}
+            </div>
+            <div className='resume-skills'>
+                <h2>Skills</h2>
+                <hr />
+                <p>{skills.join(', ')}.</p>
             </div>
         </div>
     )
